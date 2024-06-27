6feb8b2f9ea31f7b9852d67b106bc071059fb65e
nell4438 <98459805+nell4438@users.noreply.github.com>
2024-06-27 11:24:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,4 @@
 
-let cardData = [
-    {
-        site: 'STRIPE TRIAL',
-        country: 'US',
-        bin: '515462002018xxxx|09|2027|xxx',
-        link: null
-    },
-    {
-        site: 'YOUTUBE',
-        country: 'ALGERIA',
-        bin: '546775977863xxxx|02|2029|xxx',
-        link: null
-    },
-    {
-        site: 'MS 365 | COPILOT',
-        country: 'US',
-        bin: '515462003923|09|2027|xxx'
-    },
-    {
-        site: 'TIDAL',
-        country: 'US',
-        bin: '4815830040|03|2029|xxx',
-        link: null
-    },
-    {
-        site: 'APPLE TV | APPLE MUSIC',
-        country: 'MEXICO',
-        bin: '413098170337xxxx|06|2026|xxx',
-        link: 'https://redeem.services.apple/roktappletv-mx'
-    },
-      {
-        site: 'CHESS.COM',
-        country: 'US / OWN',
-        bin: '555270011808|07|2027|xxx',
-        link: null
-    },
-    {
-        site: 'PEACOCK',
-        country: 'US',
-        bin: '443547050x4xxxxx|xx|xx|xxx',
-        link: null
-    },
-    {
-        site: 'CANVA',
-        country: 'BHUTAN / THIMPU',
-        bin: '546775560834xxxx|03|32|xxx',
-        link: null
-    },
-    {
-        site: 'CRUNCHYROLL',
-        country: 'INDO ZIP 00000',
-        bin: '55769200551xxxxx|07|27|xxx',
-        link: null
-    },
-     {
-        site: 'PRIME VIDEO (CANADA)',
-        country: 'CANADA',
-        bin: '454033189113xxxx|01|27|xxx',
-        link: null
-    },
-     {
-        site: 'YOUTUBE PH',
-        country: 'OWN',
-        bin: '546775820853xxxx|06|27|xxx',
-        link: null
-    },
-     {
-        site: 'TIDAL / PLEX / STAN.AU',
-        country: 'US',
-        bin: '53635000072xxxxx|12|30|xxx',
-        link: null
-    },
-     {
-        site: '2D SITE (BUG BIN) (MC)',
-        country: 'RANDOM',
-        bin: '5425504502xxxxxx|xx|xx|xxx',
-        link: null
-    },
-     {
-        site: 'YOUTUBE US',
-        country: 'US',
-        bin: '49987100012xxxxx|xx|xx|xxx',
-        link: null
-    },
-    {
-        site: 'FUBO.TV',
-        country: 'US',
-        bin: '55750100000xxxxx|xx|xx|xxx',
-        link: null
-    },
-
-];
 document.addEventListener('DOMContentLoaded', function () {
     var monthDropdown = document.getElementById('monthDropdown');
     var yearDropdown = document.getElementById('yearDropdown');
@@ -272,7 +180,9 @@ function showToast(checkDigit) {
 
 
 $(document).ready(function () {
-
+    // axios.get(`https://randomuser.me/api/1.4/?nat=us`).then((res) => {
+    //     console.log(res.data.results[0])
+    // })
     $('#binListModal').on('show.bs.modal', function (event) {
         displayCards(cardData);
     });
